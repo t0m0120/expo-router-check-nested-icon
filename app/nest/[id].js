590e51e9832b41a2c4e96b041d20d9206ed5c92d
t0m0120 <highwest.t@gmail.com>
2023-01-21 22:47:02 +0900

    add nest/icon

---
+++ app/nest/[id].js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View >
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
+    </View>
+  );
+}
